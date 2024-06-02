935e201dfa0b4918aa63a36b13d373773f0e99c9
sajid islam <sajidisalm941@gmail.com>
2024-06-02 21:17:23 +0600

    added navbar and emplement theme toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins:"'Poppins', sans-serif",
+      },
+      colors:{
+        mainColor: '#ff6700',
+        dark:'#1d232a'
+      }
+    },
   },
   plugins: [require('daisyui'),],
 }
